5615edf9e756acf19b2494cffb4e0b757bd1bd0b
İpek Nezihe Can <147480958+ipekncan@users.noreply.github.com>
2026-07-02 13:24:28 +0300

    File updated at 02.07.2026 13:24:28

--- https:/github.com/ipekncan/leetcode-grind.git/1-TwoSum/1-TwoSum.c
+++ https:/github.com/ipekncan/leetcode-grind.git/1-TwoSum/1-TwoSum.c
@@ -1,22 +1,42 @@
-// Last updated: 24.06.2026 13:29:06
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int* result = (int*)malloc(2 * sizeof(int));
-    *returnSize=2;
-    for(int i=0;i<numsSize;i++){
-        for(int j=i+1;j<numsSize;j++){
-           if(nums[i]+nums[j]==target){
-            result[0]=i;
-            result[1]=j;
-            return result;
-           }
-        }
-    }
-   *returnSize = 0;
-    return NULL;
-}
+// Last updated: 02.07.2026 13:24:28
+1bool isPalindrome(int x) {
+2    
+3    char str[12];
+4    snprintf(str, sizeof(str), "%d", x);
+5    int left=0;
+6    int right=0;
+7    int len = strlen(str);
+8
+9
+10    if(x<10 && x>=0) return true;
+11    
+12    
+13   
+14
+15     if (len % 2 != 0) {
+16        left = len / 2;
+17        right = len / 2;
+18    } 
+19     else {
+20        left = (len / 2) - 1;
+21        right = len / 2;
+22    }
+23
+24
+25    
+26       
+27        while(left>=0 && right<strlen(str))
+28        {
+29        
+30            if(str[left]!=str[right]){
+31                return false;
+32                 
+33            }
+34            left--;
+35            right++;
+36        }
+37        
+38        
+39    
+40    return true;
+41}
